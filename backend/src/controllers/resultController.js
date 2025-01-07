1bb1051b5476044e2cf65047851e2ef642832cb3
AbdelrahmanAbbas1 <abdelrahmanabbas599@gmail.com>
2025-01-08 00:40:58 +0200

    Adding new file

--- backend/src/controllers/resultController.js
+++ backend/src/controllers/resultController.js
@@ -2,11 +2,11 @@ const pool = require('../../config/db');
 
 exports.getUserResults = async (req, res) => {
   const { userId } = req.params;
-  try {
-    const [results] = await pool.query('SELECT * FROM results WHERE user_id = ?', [userId]);
-    res.status(200).json(results);
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Failed to retrieve user results" });
-  }
+  // try {
+  //   const [results] = await pool.query('SELECT * FROM results WHERE user_id = ?', [userId]);
+  //   res.status(200).json(results);
+  // } catch (err) {
+  //   console.error(err);
+  //   res.status(500).json({ error: "Failed to retrieve user results" });
+  // }
 };
